d6ba65aefa1def520eb1d0773530e71e72941900
mvqcode <ivansoriatest@gmail.com>
2020-10-19 23:48:40 -0500

    con uls dinamicas para el menu de categorias

--- main/views.py
+++ main/views.py
@@ -20,43 +20,16 @@ from django.utils import timezone
 # from django.core.urlresolvers import reverse_lazy
 import hashlib
 
-from .models import Preferencia, Noticia, Perfil
+from .models import Preferencia, Noticia, Perfil, Fuente, Categoria
 
 from .forms import SignUpForm
 
-# Create your views here.
-
-app_name = "main"
-
-# PARA LEER NOTICIAS INTERNACIONALES, NO SE UTILIZAR´NA POR EL MOMENTO
-
-secret = "5f039f3f100a42cdbc4aa9661082475f"
-
-# Define the endpoint
-url = 'http://newsapi.org/v2/everything?'
-
-# Specify the query and
-# number of returns
-parameters = {
-    'q': 'rebrote',  # query phrase
-    'pageSize': 100,  # maximum is 100
-    'apiKey': secret,  # your own API key
-    'language': 'es',
-
-}
-
-# Make the request
-response = requests.get(url,
-                        params=parameters)
-
-# Convert the response to
-# JSON format and pretty print it
-response_json = response.json()
-
-# Clase que contiene cada artículo de noticia
-
 
 class articulo:
+    '''
+    Clase que contiene temporalmente cada artículo de noticias
+    '''
+
     def __init__(self, article):
         h = hashlib.md5()
         h.update(article['title'].encode('utf-8'))
@@ -70,63 +43,14 @@ class articulo:
         self.nombre = article['source']['name']
 
 
-def extraerArticulosAPI(response_json):
-    for a in response_json['articles']:
-        yield(articulo(a))
-
-
-articulos1 = list(extraerArticulosAPI(response_json))
-
-# ESTAS LISTAS DEBEN CARGARSE A UNA BASE DE DATOS Y SER DEVUELTAS EN ESTE MISMO FORMATO
-# Lista de páginas de noticias aceptadas, rss funciona, se debe revisar antes de agregar una fuente
-feedsPolitica = [
-    {'diario': 'El Comercio',
-        'urlfeed': "https://elcomercio.pe/arcio/rss/category/politica/"},
-    {'diario': 'La República',
-        'urlfeed': "https://larepublica.pe/rss/politica.xml?outputType=rss"},
-    {'diario': 'Peru.com',
-        'urlfeed': "https://peru.com/feed/actualidad/politicas"},
-    {'diario': 'Diario Correo',
-        'urlfeed': "https://diariocorreo.pe/arcio/rss/category/politica/?ref=dcr"},
-    # {'diario': 'El Perfil',
-    #     'urlfeed': "https://elperfil.pe/politica/feed/"},
-]
-
-feedsSalud = [
-    {'diario': 'Peru.com',
-        'urlfeed': "https://peru.com/feed/estilo-de-vida/salud"},
-    {'diario': 'Diario Correo',
-        'urlfeed': "https://diariocorreo.pe/arcio/rss/category/salud/?ref=dcr"},
-    {'diario': 'La República',
-        'urlfeed': "https://larepublica.pe/rss/salud.xml?outputType=rss"},
-    # {'diario': 'El Perfil',
-    #     'urlfeed': "https://elperfil.pe/salud/feed/"},
-]
-
-feedsEconomia = [
-    {'diario': 'Peru.com',
-        'urlfeed': "https://peru.com/feed/actualidad/economia-y-finanzas"},
-    {'diario': 'Diario Correo',
-        'urlfeed': "https://diariocorreo.pe/arcio/rss/category/economia/?ref=dcr"},
-    {'diario': 'La República',
-        'urlfeed': "https://larepublica.pe/rss/economia.xml?outputType=rss"},
-    # {'diario': 'El Perfil',
-    #     'urlfeed': "https://elperfil.pe/economia/feed/"},
-    {'diario': 'El Comercio',
-        'urlfeed': "https://elcomercio.pe/arcio/rss/category/economia/"},
-]
-
-# Extrae los artículos del json de feed para cargarlos en la clase Noticia
-
-
 def extraerArticulos(feeds):
     '''
-    Recibe una lista de feeds cada item de la lista tiene los atributos 'diario' y 'urlfeed'
+    Recibe una lista de feeds cada item de la lista tiene los atributos 'diario' y 'url'
     Devuelve un iterable conteniendo los objetos articulo obtenidos del feed
     '''
     for feed in feeds:
         # Lee el RSS contenido en el feed y obteien un diccionario donde cada entrada es un dato del artículo
-        rss = feedparser.parse(feed['urlfeed'])
+        rss = feedparser.parse(feed['url'])
         for item in rss['entries']:
             article = dict()
             if 'title' in item:
@@ -141,28 +65,26 @@ def extraerArticulos(feeds):
                 article['source']['name'] = feed['diario']
                 yield articulo(article)
 
-# categorias consignadas, 'nomcat' contiene el nombre y textcat el texto que se mostrará en el menú
 
+def obtenerFuentes(categorias):
+    articulos = []  # aqui se acumularan los objetos articulo
+    # obtiene todas las fuentes de la base de datos
+    # aqui se guardaran las fuentes en un diccionario por categorias
+    todasFuentesCategoria = {}
+    for categoria in categorias:
+        # seleccion por un Foreign Key
+        fuentes = Fuente.objects.filter(
+            categoria__categoria=categoria['categoria']).values()
+        articulosCategoria = list(extraerArticulos(fuentes))
+        articulosCategoria.sort(key=lambda x: x.fecha, reverse=True)
+        articulos = articulos + articulosCategoria
+        todasFuentesCategoria.setdefault(
+            categoria['categoria'], articulosCategoria)
 
-categorias = [
-    {'nomcat': 'politica', 'textcat': 'Política'},
-    {'nomcat': 'economia', 'textcat': 'Economía'},
-    {'nomcat': 'salud', 'textcat': 'Salud'},
-    # {'nomcat': 'recomendados', 'textcat': 'Recomendados para ti'},
-]
+    return articulos, todasFuentesCategoria
 
-articulosPolitica = list(extraerArticulos(feedsPolitica))
-articulosSalud = list(extraerArticulos(feedsSalud))
-articulosEconomia = list(extraerArticulos(feedsEconomia))
-articulos = articulosPolitica + articulosSalud + articulosEconomia
 
 # devuelve el objeto artículo de una lista de artículo
-
-
-def recuperaNoticia(articulos, idn):
-    for articulo in articulos:
-        if articulo.id == int(idn):
-            return articulo
 
 
 def grabaNoticia(articulo):
@@ -184,63 +106,54 @@ def grabaNoticia(articulo):
     return instancia
 
 
-# ----------- SECUENCIA PARA GRABAR TODOS LOS ARTICULOS ----------------
-# for articulo in articulos:
-#     consulta = Noticia.objects.filter(id_noticia=articulo.id)
-#     instancia = None
-#     if consulta.count() == 0:
-#         instancia = Noticia.objects.create(
-#             id_noticia=articulo.id,
-#             titulo=articulo.titulo,
-#             descripcion=articulo.descripcion,
-#             url=articulo.url,
-#             urlImagen=articulo.urlImagen,
-#             fecha=articulo.fecha,
-#             autor=articulo.autor,
-#             nombre=articulo.nombre,
-#         )
-#     else:
-#         instancia = consulta[0]
-#     instancia.save()
+def calculaDistancias():
 
-articulosPolitica.sort(key=lambda x: x.fecha, reverse=True)
-articulosSalud.sort(key=lambda x: x.fecha, reverse=True)
-articulosEconomia.sort(key=lambda x: x.fecha, reverse=True)
-articulos.sort(key=lambda x: x.fecha, reverse=True)
+    articulos.sort(key=lambda x: x.fecha, reverse=True)
 
-f = open("datos.csv", "w", encoding="utf-8")
-f.write('id,description\n')
-for articulo in articulos:
-    linea = "" + str(articulo.id) + ",\"" + articulo.titulo + \
-        "-" + articulo.descripcion + "\"\n"
-    f.write(linea)
-f.close()
+    f = open("datos.csv", "w", encoding="utf-8")
+    f.write('id,description\n')
+    for articulo in articulos:
+        linea = "" + str(articulo.id) + ",\"" + articulo.titulo + \
+            "-" + articulo.descripcion + "\"\n"
+        f.write(linea)
+    f.close()
 
+    def normalizatexto(texto):
+        '''
+        Normaliza el texto para evitar que caracteres extraños entren al conteo de palabras
+        '''
+        return unicodedata.normalize('NFKD', texto).encode('ASCII', 'ignore').strip().lower()
 
-def normalizatexto(texto):
-    return unicodedata.normalize('NFKD', texto).encode('ASCII', 'ignore').strip().lower()
+    # Prepara el vector de puntajes para hacer las recomendaciones
 
+    ds = pd.read_csv("datos.csv", usecols=[
+                     "id", "description"], encoding="latin")
 
-ds = pd.read_csv("datos.csv", usecols=["id", "description"], encoding="latin")
+    tf = TfidfVectorizer(analyzer='word', ngram_range=(1, 3),
+                         min_df=0, stop_words='english')
+    tfidf_matrix = tf.fit_transform(ds['description'])
 
-tf = TfidfVectorizer(analyzer='word', ngram_range=(1, 3),
-                     min_df=0, stop_words='english')
-tfidf_matrix = tf.fit_transform(ds['description'])
+    cosine_similarities = linear_kernel(tfidf_matrix, tfidf_matrix)
 
-cosine_similarities = linear_kernel(tfidf_matrix, tfidf_matrix)
+    results = {}
 
-results = {}
+    for idx, row in ds.iterrows():
+        similar_indices = cosine_similarities[idx].argsort()[:-100:-1]
+        similar_items = [(cosine_similarities[idx][i], ds['id'][i])
+                         for i in similar_indices]
 
-for idx, row in ds.iterrows():
-    similar_indices = cosine_similarities[idx].argsort()[:-100:-1]
-    similar_items = [(cosine_similarities[idx][i], ds['id'][i])
-                     for i in similar_indices]
+        results[row['id']] = similar_items[1:]
 
-    results[row['id']] = similar_items[1:]
+    return results
 
 
-def item(id):
-    return ds.loc[ds['id'] == id]['description'].tolist()[0].split(' - ')[0]
+categorias = Categoria.objects.all().values()
+articulos, todosXcategoria = obtenerFuentes(categorias)
+results = calculaDistancias()
+
+
+# def item(id):
+#     return ds.loc[ds['id'] == id]['description'].tolist()[0].split(' - ')[0]
 
 
 def recommend(item_id, num):
@@ -262,7 +175,6 @@ class SignUpView(CreateView):
             usuario = form.save()
             nombre_usuario = form.cleaned_data.get(
                 'username')  # obtiene el nombre del usuario
-            # crea un mensaje para el usuario
             messages.success(
                 self.request, f"Nueva Cuenta Creada : {nombre_usuario}")
             login(self.request, usuario)
@@ -273,28 +185,13 @@ class SignUpView(CreateView):
             for msg in form.error_messages:
                 messages.error(
                     self.request, f"{msg}: {form.error_messages[msg]}")
-        # usuario = authenticate(username=usuario, password=password)
-        # return redirect('/')
         return render(self.request, "main/perfil_form.html", {"form": form})
 
     def form_invalid(self, form):
-        print("ERRORES", len(form.error_messages))
         for msg in form.error_messages:
-            print("ERROR:", msg)
             messages.error(
                 self.request, f"{form.error_messages[msg]}")
         return render(self.request, "main/perfil_form.html", {"form": form})
-
-
-class BienvenidaView(TemplateView):
-    '''
-    Muestra una ventana de bienvenida, con información personal del usuario
-    '''
-    template_name = 'main/bienvenida.html'
-
-
-class SignInView(LoginView):
-    template_name = 'main/login.html'
 
 
 def perfiluser(request):
@@ -317,9 +214,9 @@ def categoria(request, nomcat=None):
     consultaPreferencias = Preferencia.objects.filter(
         usuario=usuario_actual)
     preferencias = [
-        preferencia.id_noticia for preferencia in consultaPreferencias]
+        preferencia.id_noticia.id_noticia for preferencia in consultaPreferencias]
 
-    return HttpResponse(str(nomcat))
+    return render(request, "main/inicio.html", {"news": todosXcategoria[nomcat], "categoria": nomcat, "activa": nomcat, "categorias": categorias, "preferencias": preferencias, })
 
 
 def politica(request):
@@ -370,7 +267,7 @@ def recomendados(request):
         usuario=usuario_actual)
     preferencias = [
         preferencia.id_noticia.id_noticia for preferencia in consultaPreferencias]
-    print("Preferencias:", consultaPreferencias)
+    # print("Preferencias:", consultaPreferencias)
     if len(preferencias) == 0:  # si no hay preferencias, puede significar que nunca eligió nada o que no se ha logueado
         idUltimaNoticia = None
     else:
@@ -386,23 +283,28 @@ def recomendados(request):
     ids_recomendados = [item[1] for item in recomendaciones]
     articulosrecomendados = [
         articulo for articulo in articulos if articulo.id in ids_recomendados and not articulo.id in preferencias]
-    print("PREFERENCIAS", preferencias)
-
-    print("ARTICULO", [art.id for art in articulosrecomendados])
+    # print("PREFERENCIAS", preferencias)
+    # print("ARTICULO", [art.descripcion for art in articulosrecomendados])
     return render(request, "main/inicio.html", {"news": articulosrecomendados, "categoria": "recomendados", "activa": "recomendados", "categorias": categorias, "preferencias": preferencias, })
 
 
 def check(request):
+    def recuperaNoticia(articulos, idn):
+        for articulo in articulos:
+            if articulo.id == int(idn):
+                return articulo
     noticia = request.POST['id']
     if noticia == '':
         return HttpResponse('')
     # check = request.POST['click']
+    print("ID:", noticia)
     usuario = request.POST['usuario']
     consulta = Preferencia.objects.filter(
         usuario=usuario).filter(id_noticia=noticia)
     if (len(consulta) == 0):
         nuevanoticia = recuperaNoticia(
             articulos, noticia)  # obtiene el objeto noticia
+        print("NOTICIA:", nuevanoticia)
         noticiagrabar = grabaNoticia(nuevanoticia)
         nuevocheck = Preferencia.objects.create(
             id_noticia=noticiagrabar, preferencia=True, usuario=usuario)
@@ -430,7 +332,7 @@ def registro(request):
             return redirect("main:homepage")
         else:
             for msg in form.error_messages:
-                print("ERROR:", msg)
+                # print("ERROR:", msg)
                 messages.error(request, f"{msg}: {form.error_messages[msg]}")
 
     return render(request, "main/registro.html", {"form": form})
